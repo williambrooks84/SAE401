217f04d94641e5d03d36f892b5064e3cf233732b
William Brooks <william.brooks@etu.unilim.fr>
2025-03-19 11:38:10 +0100

    Tailwindcss fonctionnel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import Home from "./components/Home"
 import Login from "./components/Login";
-import Signup from "./components/Signup"; // Ensure this import is correct
+import Signup from "./components/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element = {<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
